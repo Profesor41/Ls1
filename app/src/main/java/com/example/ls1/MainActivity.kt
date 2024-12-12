19b7b53ec3b36abac49da0b4e34536eb241fc297
Profesor41 <149595806+Profesor41@users.noreply.github.com>
2024-12-12 16:04:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ls1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ls1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val label = findViewById<TextView>(R.id.main_label)
+        val userData: EditText = findViewById(R.id.user_data)
+        val button: Button = findViewById(R.id.button)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
