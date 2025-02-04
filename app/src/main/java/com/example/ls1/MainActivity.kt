6034dc8694167bfd6adeaecc8af8e41a2c16c253
Evhen Berezhnyi <149595806+Profesor41@users.noreply.github.com>
2025-02-04 14:04:19 +0200

    First run
    All ok

--- app/src/main/java/com/example/ls1/MainActivity.kt
+++ app/src/main/java/com/example/ls1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +12,26 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Встановлюємо макет активності
+        setContentView(R.layout.activity_main)
+
+        // Знаходимо елементи після setContentView
         val label = findViewById<TextView>(R.id.main_label)
         val userData: EditText = findViewById(R.id.user_data)
         val button: Button = findViewById(R.id.button)
-        super.onCreate(savedInstanceState)
+
+        button.setOnClickListener {
+            val text = userData.text.toString().trim()
+            if (text == "toast") {
+                Toast.makeText(this, "User enter toast", Toast.LENGTH_SHORT).show()
+            } else {
+                label.text = text
+            }
+        }
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
