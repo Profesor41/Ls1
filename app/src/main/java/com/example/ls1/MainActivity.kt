35a1b8ed72238cb0e2470146b263c896fea0dbb7
Evhen Berezhnyi <149595806+Profesor41@users.noreply.github.com>
2025-02-04 15:21:09 +0200

    For Test

--- app/src/main/java/com/example/ls1/MainActivity.kt
+++ app/src/main/java/com/example/ls1/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Встановлюємо макетіще активності
+        // Встановлюємо макет активності:
         setContentView(R.layout.activity_main)
 
         // Знаходимо елементи після setContentView
