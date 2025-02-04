3852fce52d58f4c8744a19c1ec1efc74cccb2ec4
Evhen Berezhnyi <149595806+Profesor41@users.noreply.github.com>
2025-02-04 14:35:10 +0200

    Push2
    Add list and option to delete it by click

--- app/src/main/java/com/example/ls1/MainActivity.kt
+++ app/src/main/java/com/example/ls1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ls1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -14,21 +16,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Встановлюємо макет активності
+        // Встановлюємо макетіще активності
         setContentView(R.layout.activity_main)
 
         // Знаходимо елементи після setContentView
-        val label = findViewById<TextView>(R.id.main_label)
+        val listView = findViewById<ListView>(R.id.listview)
         val userData: EditText = findViewById(R.id.user_data)
         val button: Button = findViewById(R.id.button)
 
+        val todos:MutableList<String> = mutableListOf()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            val text = listView.getItemAtPosition(i). toString()
+            adapter.remove(text)
+
+            Toast.makeText(this, "Ми видалили: $text", Toast.LENGTH_LONG). show()
+        }
+
         button.setOnClickListener {
             val text = userData.text.toString().trim()
-            if (text == "toast") {
-                Toast.makeText(this, "User enter toast", Toast.LENGTH_SHORT).show()
-            } else {
-                label.text = text
-            }
+            if(text !="")
+                adapter.insert(text, 0)
         }
 
         enableEdgeToEdge()
